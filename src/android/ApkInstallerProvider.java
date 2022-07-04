70aaeaa422505efc07826de99670fa9b61967eb4
oussamak25 <91186771+oussamak25@users.noreply.github.com>
2022-07-04 14:05:33 +0200

    Delete src/android directory

--- src/android/ApkInstallerProvider.java
+++
@@ -1,7 +0,0 @@
-package plugin.apkInstaller;
-
-import androidx.core.content.FileProvider;
-
-public class ApkInstallerProvider extends FileProvider {
-   // avoid android:authrities conflict with other lib which also use android.support.v4.content.FileProvider
-}
